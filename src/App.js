ea162f307a68840113a57891e95d5281ac7c0c0f
realjck <jckleinbourg@gmail.com>
2023-06-13 14:27:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import VisitRow from './components/VisitRow';
 
-function App() {
+function App(){
+  // state
+
+  // behave
+
+  // render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Visits Back-office</h1>
+        <VisitRow/>
     </div>
   );
 }
---
+++ src/components/VisitRow.jsx
@@ -0,0 +1,17 @@
+
+
+export default function VisitRow(){
+    // state
+
+    // behave
+
+    // render
+    return (
+        <div className="visit-row">
+            <span>13 juin 2023</span>
+            <span>Real Burger</span>
+            <span>(Real Burger)</span>
+            <span>17mn</span>
+        </div>
+    );
+}
