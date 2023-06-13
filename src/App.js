f24e46b668dfaf074b4487a862b1391392a27820
realjck <jckleinbourg@gmail.com>
2023-06-13 18:24:50 +0200

    look and feel

--- src/App.js
+++ src/App.js
@@ -8,10 +8,39 @@ function App(){
   // behave
 
   // render
+
+  const renderList = () => {
+    const components = [];
+
+    for (let i = 0; i < 100; i++) {
+      components.push(<VisitRow key={i} />);
+    }
+
+    return components;
+  };
+
   return (
     <div>
-      <h1>Visits Back-office</h1>
-        <VisitRow/>
+        <div className='container'>
+          <div className='back-office-header'>
+            <h1>visits back-office</h1>
+            <div className='back-office-header-navbar'>
+              <div className='active back-office-header-date'>
+                <button>📅</button><img src="/arrow.svg" alt="arrow"/>
+              </div>
+              <div className="back-office-header-name">
+                <button>👥</button><img src="/arrow.svg" alt="arrow"/>
+              </div>
+              <div className="back-office-header-duration">
+                <button>⌚️</button><img src="/arrow.svg" alt="arrow"/>
+              </div>
+            </div>
+          </div>
+          <div className='back-office-body'>
+            {renderList()}
+          </div>
+          
+        </div>
     </div>
   );
 }
--- src/components/VisitRow.jsx
+++ src/components/VisitRow.jsx
@@ -1,17 +1,25 @@
-
+import './VisitRow.css';
+import { useState } from 'react';
 
 export default function VisitRow(){
     // state
-
+    const [visit, setVisit] = useState({
+        date: "13 juin 2023",
+        displayName: "Real",
+        userName: "Real Burger",
+        duration: "17mn"
+    });
     // behave
 
     // render
     return (
         <div className="visit-row">
-            <span>13 juin 2023</span>
-            <span>Real Burger</span>
-            <span>(Real Burger)</span>
-            <span>17mn</span>
+            <div className='visit-row-date'>{visit.date}</div>
+            <div className='visit-row-name'>
+                <span className='visit-row-displayname'>{visit.displayName}</span>
+                <span className='visit-row-username'>({visit.userName})</span>
+            </div>
+            <div className='visit-row-duration'>{visit.duration}</div>
         </div>
     );
 }
